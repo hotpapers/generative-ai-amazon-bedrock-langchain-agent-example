1798f77cc11186639882abf8c0b7e0ef9e2d3129
chachenc <chachenc@amazon.com>
2024-05-18 22:04:43 -0700

    integrate with kommunicate bot

---
+++ shell/set-up-env-var.sh
@@ -0,0 +1,8 @@
+
+export AMPLIFY_REPOSITORY=https://github.com/hotpapers/generative-ai-amazon-bedrock-langchain-agent-example  # Forked repository URL from Pre-Deployment (Exclude '.git' from repository URL)
+export GITHUB_PAT=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  # GitHub PAT copied from Pre-Deployment
+export STACK_NAME=bedrock-langchain-agent-stack  # Stack name must be lower case for S3 bucket naming convention
+export KENDRA_WEBCRAWLER_URL=https://github.com  # Public or internal HTTPS website for Kendra to index via Web Crawler (e.g., https://www.investopedia.com/) - Please see https://docs.aws.amazon.com/kendra/latest/dg/data-source-web-crawler.html
+export AWS_REGION=us-west-2  # Stack deployment region
+
+
